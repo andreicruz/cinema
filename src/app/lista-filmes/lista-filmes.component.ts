fbb3688d47d1fc97dc4432033645266b70439215
Andrei Cruz <andrei_cruz97@hotmail.com>
2017-10-24 00:32:33 -0200

    Adicionado metodo de salvar favorito

--- src/app/lista-filmes/lista-filmes.component.ts
+++ src/app/lista-filmes/lista-filmes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Filme } from "../filme";
 import { CrudFilmesService } from "../crud-filmes.service";
+import { FavoritosService} from "../favoritosService/favoritos.service";
 
 @Component({
   selector: 'app-lista-filmes',
@@ -11,14 +12,18 @@ export class ListaFilmesComponent implements OnInit {
   titulo = "Lista de Filmes";
   filmes: Filme[]=[];
 
-  constructor(private servico:CrudFilmesService) { }
+  constructor(private servico:CrudFilmesService,
+              private servicoFavoritos: FavoritosService) { }
 
   ngOnInit() {
     this.filmes = this.servico.getFilmes();
   }
 
+  salvarFavorito(filme:Filme){
+    this.servicoFavoritos.adicionarFavorito(filme);
+  }
+
   remover(filme:Filme){
     this.servico.removerFilme(filme);
   }
-
 }
